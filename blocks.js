2b9457ec8692ec64bf44e663ff53c51c713ea5a1
sharkbot-neko <modoggu12345@outlook.com>
2025-11-24 13:42:03 +0900

    音楽停止を追加、DM送信を追加

--- blocks.js
+++ blocks.js
@@ -16,10 +16,18 @@ function generateBlocklyBlcok(category, labelText, type) {
 }
 
 window.onload = function() {
-    const test = generateBlocklyCategory('テストなう', '#000000');
-    generateBlocklyBlcok(test, "テストですねぇ", "test_now");
+    // 音楽など(VC)
     const music = generateBlocklyCategory('VC', '#ff0a0a');
     generateBlocklyBlcok(music, "音楽を再生", "vc_play");
+    generateBlocklyBlcok(music, "音楽を停止", "vc_stop");
+
+    // DMに送信
+    const dm = generateBlocklyCategory('DM', '#07d3b1');
+    generateBlocklyBlcok(dm, "実行者のDMに送信", "dm_send_runner");
+
+    // 定番コマンド
+    const templates = generateBlocklyCategory('定番コマンド', '#533939');
+    generateBlocklyBlcok(templates, "Pingコマンドを追加", "pingcommand_add");
 
     chrome.runtime.sendMessage({ action: "injectBlocklyBlock" });
 };
--- codes.js
+++ codes.js
@@ -4,17 +4,6 @@ function addBlock() {
         return;
     }
 
-    window.Blockly.Blocks['test_now'] = {
-        init: function() {
-            this.appendDummyInput().appendField("テストなう"); this.setPreviousStatement(true, null); this.setNextStatement(true, null); this.setColour(160);
-        }
-    };
-
-    window.Blockly.Python['test_now'] = function(block) {
-        const code = 'print("テストなう！")';
-        return code; 
-    };
-
     window.Blockly.Blocks['vc_play'] = {
         init: function() {
             this.appendDummyInput().appendField("音楽を再生"); this.setPreviousStatement(true, null); this.setNextStatement(true, null); this.setColour(160);
@@ -24,6 +13,40 @@ function addBlock() {
 
     window.Blockly.Python['vc_play'] = function(block) {
         const code = `voice_client = ctx.guild.voice_client\nif voice_client is None:\n return\naudio_source = discord.FFmpegPCMAudio(source=${Blockly.Python.valueToCode(block, 'MP3_PATH', Blockly.Python.ORDER_NONE)})\nif not voice_client.is_playing():\nvoice_client.play(audio_source)`;
+        return code;
+    };
+
+    window.Blockly.Blocks['vc_stop'] = {
+        init: function() {
+            this.appendDummyInput().appendField("音楽を停止"); this.setPreviousStatement(true, null); this.setNextStatement(true, null); this.setColour(160);
+        }
+    };
+
+    window.Blockly.Python['vc_stop'] = function(block) {
+        const code = `voice_client = ctx.guild.voice_client\nif voice_client is None:\n    return\nvoice_client.stop()`;
+        return code;
+    };
+
+    window.Blockly.Blocks['dm_send_runner'] = {
+        init: function() {
+            this.appendDummyInput().appendField("実行者のDMに送信"); this.setPreviousStatement(true, null); this.setNextStatement(true, null); this.setColour(160);
+            this.appendValueInput("MESSAGE").setCheck("String").appendField("メッセージ内容");
+        }
+    };
+
+    window.Blockly.Python['dm_send_runner'] = function(block) {
+        const code = `await ctx.author.send(${Blockly.Python.valueToCode(block, 'MESSAGE', Blockly.Python.ORDER_NONE)})`;
+        return code;
+    };
+
+    window.Blockly.Blocks['pingcommand_add'] = {
+        init: function() {
+            this.appendDummyInput().appendField("Pingコマンドを追加"); this.setPreviousStatement(true, null); this.setNextStatement(true, null); this.setColour(160);
+        }
+    };
+
+    window.Blockly.Python['pingcommand_add'] = function(block) {
+        const code = `@commands.command(name="ping")\nasync def ping_command(self, ctx: commands.Context):\n    await ctx.reply('Pong! {round(self.bot.latency * 1000)}ms')`;
         return code;
     };
 
